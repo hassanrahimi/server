33d6d261facb526f1cbbc4fb6a707cedfe1b3eaa
hassanrahimi <37825598+hassanrahimi@users.noreply.github.com>
2023-04-05 03:21:20 +0430

    chagne app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         please dont wordt
+         please dont wordt sfsdfsdfsdfdsfdsffdsdsf
         </p>
         <a
           className="App-link"
